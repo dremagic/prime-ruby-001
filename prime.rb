35495c1d6cf986f10bc42012af37d635928cd466
dremagic <deandre.ellison@gmail.com>
2017-03-16 15:12:09 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(number)
+if number>1
+  test_array = (2...number).to_a
+  check_array = []
+  test_array.each do |test_number|
+    check_array << number%test_number
+  end
+if check_array.include?(0)
+  false
+else
+  true
+end
+else
+  return false
+end
+
+end
